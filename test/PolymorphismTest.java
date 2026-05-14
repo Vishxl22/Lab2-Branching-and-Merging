1c4f76a18f78da4e9e8dfc8a87025d403779e7b2
Vishxl22 <packiyarajahvishal@gmail.com>
2026-05-14 19:39:01 -0400

    saving local changes

--- test/PolymorphismTest.java
+++ test/PolymorphismTest.java
@@ -8,7 +8,7 @@ public class PolymorphismTest {
         Person person = new Person("Somebody");
         assertEquals("General", Polymorphism.getStudentRole(person));
 
-        Student student = new Student("Someone", "207207207");;
+        Student student = new Student("Someone", "207207207");
         assertEquals("Student", Polymorphism.getStudentRole(student));
 
     }
